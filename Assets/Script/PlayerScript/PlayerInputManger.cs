b7f70780553929791d295f53e105cc820a58d78d
albinputte <woleycraft@gmail.com>
2025-04-09 16:14:26 +0200

    Made a Generic Input buffer

--- Assets/Script/PlayerScript/PlayerInputManger.cs
+++ Assets/Script/PlayerScript/PlayerInputManger.cs
@@ -1,18 +1,30 @@
 using SmallHedge.SoundManager;
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Rendering;
 
 public class PlayerInputManger : MonoBehaviour
 {
-
+    [SerializeField] private float bufferTime = 0.2f;
     private Vector3 rawInput;
     public float normInputX;
     public float normInputY;
     public bool isSprinting;
     public bool IsAttacking;
     public bool ActionPefromed;
+    private bool CanPefromKeyBuffer;
+    [SerializeField]private Dictionary<string, KeyBuffer> BufferList = new Dictionary<string, KeyBuffer>();
+    [SerializeField] private PlayerWeaponAgent agent;
+
+    public void Start()
+    {
+        agent = FindAnyObjectByType<PlayerWeaponAgent>();
+        agent.OnExit += () => CheckBufferedInput("Fire");
+    }
 
 
     public void OnMoveInput(InputAction.CallbackContext context)
@@ -22,6 +34,14 @@ public class PlayerInputManger : MonoBehaviour
         normInputX = (int)(rawInput * Vector2.right).normalized.x;
         normInputY = (int)(rawInput * Vector2.up).normalized.y;
 
+    }
+
+    public void FixedUpdate()
+    {
+        if (!ActionPefromed && CanPefromKeyBuffer)
+        {
+            CanPefromKeyBuffer = false;
+        } 
     }
 
     public void OnSprintInput(InputAction.CallbackContext context)
@@ -42,12 +62,70 @@ public class PlayerInputManger : MonoBehaviour
     }
     public void OnAttackInput(InputAction.CallbackContext context)
     {
+        
         if (context.performed && !IsAttacking)
         {
             ActionPefromed = true;
             IsAttacking = true;
            
         }
+        if (context.canceled)
+        {
+            CanPefromKeyBuffer = true;
+        }
     }
 
+    public void OnKeyBuffer(InputAction.CallbackContext context) 
+    {
+        if (ActionPefromed && CanPefromKeyBuffer)
+        {
+            KeyBuffer buffer = new KeyBuffer();
+            buffer.TimePressed = Time.time;
+            BufferList[context.action.name] = buffer;
+            Debug.Log(context.action.name);
+
+        }
+
+    }
+
+    public void CheckBufferedInput(string actionName)
+    {
+        if (BufferList.ContainsKey(actionName))
+        {
+ 
+            float timePressed = BufferList[actionName].TimePressed;
+
+            if (Time.time <= timePressed + bufferTime)
+            {
+                Debug.Log("bUFFER");
+                BufferList.Remove(actionName); // Optionally consume it
+                StartCoroutine(PeformAttack());
+             
+            }
+            else
+            {
+                BufferList.Remove(actionName); // Expired
+            }
+        }
+
+     
+    }
+
+
+
+
+    public IEnumerator PeformAttack()
+    {
+
+        yield return new WaitForSeconds(0.1f);
+        IsAttacking = true;
+        ActionPefromed = true;
+  
+    }
+
+}
+
+public struct KeyBuffer
+{
+    public float TimePressed;
 }
