7b3f2988c010fdb3a4c427d5f200aef77f8b96a7
albinputte <woleycraft@gmail.com>
2025-04-23 16:41:24 +0200

    Fixed Attack animation reseting on next attack

--- Assets/Script/PlayerScript/PlayerWeaponAgent.cs
+++ Assets/Script/PlayerScript/PlayerWeaponAgent.cs
@@ -93,7 +93,7 @@ public class PlayerWeaponAgent : MonoBehaviour
         weaponSpriteRenderer.enabled = true;
         tempFaceDir = direction;
         facingDirection = TemporaryDirCorrection(direction);
-        CurrentAttackSpriteIndex = 0;
+      
 
         string animName = GetAnimationName(CurrentWeapon, facingDirection, currentAttackIndex);
         attackAnimator.Play(animName);
