96112610c2844a0187d60bfda5c9b9de253facb5
albinputte <woleycraft@gmail.com>
2025-05-21 10:31:35 +0200

    Crafting Button change sprite

--- Assets/Script/Crafting/CraftButtonUi.cs
+++ Assets/Script/Crafting/CraftButtonUi.cs
@@ -3,14 +3,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class CraftButtonUi : MonoBehaviour
 {
     public event Action<CraftButtonUi> OnItemClicked;
-
+    [SerializeField] private Sprite[] ButtonSprites;
+    [SerializeField] private Image Buttonimage; 
     public CraftingRecipe recipe;
     public int CurrentCraftingMangerIndex;
     public bool HasBeenClicked;
+    public void Start()
+    {
+        Buttonimage = GetComponent<Image>();
+    }
     public void SetRecipe(CraftingRecipe recipe, int CurrentCraftingMangerIndex)
     {
         this.recipe = recipe;
@@ -41,9 +47,11 @@ public class CraftButtonUi : MonoBehaviour
 
     public IEnumerator ActivateDelay()
     {
+        Buttonimage.sprite = ButtonSprites[1];
         HasBeenClicked = false;
         yield return new WaitForSeconds(0.4f);
         HasBeenClicked = true;
+        Buttonimage.sprite = ButtonSprites[0];
     }
 
 
