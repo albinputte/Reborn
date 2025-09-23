dc7965bfb5f34e5156bcc92ea0ae8ae23f320e10
albinputte <woleycraft@gmail.com>
2025-09-23 15:10:35 +0200

    Moving scenes around

--- Assets/Script/Utility/SceneField.cs
+++ Assets/Script/Utility/SceneField.cs
@@ -1,18 +1,49 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class SceneField : MonoBehaviour
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+[System.Serializable]
+public class SceneField
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private Object m_SceneAsset;
+
+    [SerializeField]
+    private string m_SceneName = "";
+    public string SceneName
     {
-        
+        get { return m_SceneName; }
     }
 
-    // Update is called once per frame
-    void Update()
+    // makes it work with the existing Unity methods (LoadLevel/LoadScene)
+    public static implicit operator string(SceneField sceneField)
     {
-        
+        return sceneField.SceneName;
     }
 }
+
+#if UNITY_EDITOR
+[CustomPropertyDrawer(typeof(SceneField))]
+public class SceneFieldPropertyDrawer : PropertyDrawer 
+{
+	public override void OnGUI(Rect _position, SerializedProperty _property, GUIContent _label)
+	{
+		EditorGUI.BeginProperty(_position, GUIContent.none, _property);
+		SerializedProperty sceneAsset = _property.FindPropertyRelative("m_SceneAsset");
+		SerializedProperty sceneName = _property.FindPropertyRelative("m_SceneName");
+		_position = EditorGUI.PrefixLabel(_position, GUIUtility.GetControlID(FocusType.Passive), _label);
+		if (sceneAsset != null)
+		{
+			sceneAsset.objectReferenceValue = EditorGUI.ObjectField(_position, sceneAsset.objectReferenceValue, typeof(SceneAsset), false); 
+
+			if( sceneAsset.objectReferenceValue != null )
+			{
+				sceneName.stringValue = (sceneAsset.objectReferenceValue as SceneAsset).name;
+			}
+		}
+		EditorGUI.EndProperty( );
+	}
+}
+#endif
